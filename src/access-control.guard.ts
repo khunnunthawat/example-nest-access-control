2faac0bbfe3755473f69075aefc718b42bbb4537
khunnunthawat <khunnunthawat@gmail.com>
2021-08-03 13:32:41 +0700

    add-nest-access-lib

---
+++ src/access-control.guard.ts
@@ -0,0 +1,48 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IQueryInfo } from 'accesscontrol';
+import { Role } from './role.interface';
+import { InjectRolesBuilder } from './decorators/inject-roles-builder.decorator';
+import { RolesBuilder } from './roles-builder.class';
+
+@Injectable()
+export class ACGuard<User extends any = any> implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    @InjectRolesBuilder() private readonly roleBuilder: RolesBuilder,
+  ) {}
+
+  protected async getUser(context: ExecutionContext): Promise<User> {
+    const request = context.switchToHttp().getRequest();
+    return request.user;
+  }
+
+  protected async getUserRoles(
+    context: ExecutionContext,
+  ): Promise<string | string[]> {
+    const user = await this.getUser(context);
+    if (!user) throw new UnauthorizedException();
+    return user.roles;
+  }
+
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
+    const roles = this.reflector.get<Role[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+
+    const userRoles = await this.getUserRoles(context);
+    const hasRoles = roles.every((role) => {
+      const queryInfo: IQueryInfo = role;
+      queryInfo.role = userRoles;
+      const permission = this.roleBuilder.permission(queryInfo);
+      return permission.granted;
+    });
+    return hasRoles;
+  }
+}
---
+++ src/decorators/user-roles.decorators.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+/**
+ * Access the user roles from the request object i.e `req.user.roles`.
+ *
+ * You can pass an optional property key to the decorator to get it from the user object
+ * e.g `@UserRoles('permissions')` will return the `req.user.permissions` instead.
+ */
+export const UserRoles = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return data ? request.user[data] : request.user.roles;
+  },
+);
